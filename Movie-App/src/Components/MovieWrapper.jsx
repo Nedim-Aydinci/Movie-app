cf26e6cc97f638958db85a5d47f7f5e39e3468f2
KristofferNR <besohackzz@gmail.com>
2026-04-15 09:14:09 +0200

    Feat: add movie card component

---
+++ Movie-App/src/Components/MovieWrapper.jsx
@@ -0,0 +1,10 @@
+import MovieCard from "./MovieCard";
+
+export default function MovieWrapper({}) {
+
+    return(
+        <section className="movie-wrapper">
+            movie.ma
+        </section>
+    )
+}
