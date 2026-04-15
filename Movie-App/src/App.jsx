d0e546b199ca8a8d7bf9c7f544bb89f2692f6681
Emil Svanström <emil.svanstrom15@gmail.com>
2026-04-15 17:29:48 +0200

    Gjort första varianten av footer, ändring kan av design kan ske senare, intallerade reacts FaIcons

--- Movie-App/src/App.jsx
+++ Movie-App/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { useState } from 'react'
+import Footer from './Components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
+
+      <Footer />
     </>
   )
 }
