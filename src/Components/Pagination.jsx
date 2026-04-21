46de51efd8ce45c24d0003110f5009fd8a158c2a
Nedim <nedim_aydinci@hotmail.com>
2026-04-21 09:56:20 +0500

    generate dynamic page numbers

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { useState } from "react";
 
-const Pagination = (currentPage, setCurrentPage) => {
+const Pagination = (
+  currentPage,
+  setCurrentPage,
+  totalMovies,
+  moviesPerPage,
+) => {
   //using state to select pages. 1 is the start, default value.
   //const [currentPage, setCurrentpage] = useState(1);
+
+  const pages = [];
+
+  for (let i = 1; i <= Math.ceil(totalMovies / moviesPerPage); i++) {
+    pages.push(i);
+  }
+
   return (
     <div>
       <button onClick={() => setCurrentpage(currentPage - 1)}>Prev</button>
-      <button>{currentPage}</button>
+      {pages.map((page) => (
+        <button key={page} onClick={() => setCurrentPage(page)}>
+          {page}
+        </button>
+      ))}
       <button onClick={() => setCurrentpage(currentPage + 1)}>Next</button>
     </div>
   );
