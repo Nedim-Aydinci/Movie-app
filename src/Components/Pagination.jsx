eb2fbe7723f86d6ac1d68a101788290abc16bf3e
Nedim <nedim_aydinci@hotmail.com>
2026-04-21 08:17:28 +0500

    create pagination component skeleton

---
+++ src/Components/Pagination.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Pagination = () => {
+  return (
+    <div>
+      <button>Prev</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>Next</button>
+    </div>
+  );
+};
+
+export default Pagination;
