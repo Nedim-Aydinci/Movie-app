8700b1f752a6bfee7f0e0b8e97adb82210987833
Nedim <nedim_aydinci@hotmail.com>
2026-04-21 08:20:24 +0500

    connect and render pagination in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import React from "react";
+import Pagination from "./Components/Pagination";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div>App</div>
+      <Pagination />
+    </>
+  );
 };
 
 export default App;
--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -13,3 +13,4 @@ const Pagination = () => {
 };
 
 export default Pagination;
+Ö;
