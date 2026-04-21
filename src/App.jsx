3d94904227d88e200a4e8f10f490807112b8e56f
Nedim <nedim_aydinci@hotmail.com>
2026-04-21 09:10:15 +0500

    currentPage state up due to requirement in app and create fetch movies using jsonfakery

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import React from "react";
 import Pagination from "./Components/Pagination";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+  //state lift up due to use state variables in app
+  const [currentPage, setCurrentpage] = useState(1);
+
+  useEffect(() => {
+    fetch("https://jsonfakery.com/movies/infinite-scroll")
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []); //empty array to prevent default rendering
   return (
     <>
       <div>App</div>
-      <Pagination />
+      <Pagination currentPage={currentPage} setCurrentpage={setCurrentpage} />
     </>
   );
 };
