fa91a18521104f4aebf8f9a634470343fdf56cf2
Nedim <nedim_aydinci@hotmail.com>
2026-04-25 10:58:06 +0500

    trailer fetch added

--- src/Components/RandomTrailer.jsx
+++ src/Components/RandomTrailer.jsx
@@ -4,7 +4,12 @@ import { useEffect, useState } from "react";
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 const RandomTrailer = () => {
+  const [trailerKey, setTrailerKey] = useState(null);
+
   useEffect(() => {
+    //early return if no movie was found
+    if (!movie) return;
+
     fetch("https://api.themoviedb.org/3/movie/popular", {
       headers: {
         Authorization: `Bearer ${TMDB_TOKEN}`,
@@ -12,6 +17,8 @@ const RandomTrailer = () => {
     })
       .then((response) => response.json())
       .then((data) => {
+        //debugging to see if it contains id or title
+        //will be removed or commented out before the final push
         console.log("movies:", data.results);
 
         // random selection
@@ -23,6 +30,27 @@ const RandomTrailer = () => {
         setMovie(randomMovie);
       });
   }, []);
+
+  useEffect(() => {
+    //early return if no movie was found
+    if (!movie) return;
+
+    fetch(`https://api.themoviedb.org/3/movie/${movie.id}/videos`, {
+      headers: {
+        Authorization: `Bearer ${TMDB_TOKEN}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("videos:", data.results);
+
+        const trailer = data.results.find(
+          (video) => video.type === "Trailer" && video.site === "YouTube",
+        );
+
+        setTrailerKey(trailer?.key);
+      });
+  }, [movie]);
   return <div>{movie?.title}</div>;
 };
 
