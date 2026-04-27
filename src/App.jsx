798d4fb4817ea8148e08a80875e9910a9dbe835c
Nedim <nedim_aydinci@hotmail.com>
2026-04-27 12:36:05 +0500

    app.jsx content adjusted with route logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./Pages/NotFound";
 import Home from "./Pages/Home";
 
-const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
-
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  //state lift up due to use state variables in app
-  const [currentPage, setCurrentPage] = useState(1);
-  //totalPages implemented after changing api-source
-  const [totalPages, setTotalPages] = useState(1);
-
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?with_original_language=en&page=${currentPage}&sort_by=popularity.desc`,
-      {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${TMDB_TOKEN}`,
-          accept: "application/json",
-        },
-      },
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        //to display less movieCard "slice" implemented to results
-        setMovies(data.results.slice(0, 12)); // <-- change with respect to page layout
-        setTotalPages(data.total_pages);
-      });
-  }, [currentPage]); //page based render
-
   return (
-    <>
-      <MovieWrapper movies={movies} />;
-      <Pagination
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalPages={totalPages}
-      />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
