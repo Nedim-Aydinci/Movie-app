9fe4023305b4f6433e69d25b247911b73d5b9e59
emil7233 <emil.svanstrom15@gmail.com>
2026-04-29 23:57:05 +0200

    Added a fetch and display movie details

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,12 +1,32 @@
 import { useParams } from "react-router"
+import { useState, useEffect } from "react"
+import MovieInfoCard from "../Components/MovieInfoCard"
+
+const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export default function MoviePage() {
 
     const { id } = useParams()
+    const [movie, setMovie] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${TMDB_TOKEN}`,
+                accept: "application/json",
+            },
+        })
+        .then((res) => res.json())
+        .then((data) => setMovie(data))
+        .catch((error) => console.error("Error fetching movie", error))
+    }, [id])
+
+    if(!movie) return <p>Laddar...</p>
     
     return (
         <>
-        
+        <MovieInfoCard movie={movie} />
         </>
     )
 }
