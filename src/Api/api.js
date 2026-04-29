d0d79c07c3083e3a016d8d4e31517018cd9026c6
fiag84 <229909100+fiag84@users.noreply.github.com>
2026-04-29 15:47:13 +0200

    collects all fetch functions in one file

---
+++ src/Api/api.js
@@ -0,0 +1,20 @@
+//RandomMovie in Navbar
+export async function fetchRandomMovie() {}
+
+//Search in Navbar
+export async function searchMovies() {}
+
+//FavoritesPage + MoviePage
+export async function fetchMovieById() {}
+
+//RandomTrailer
+export async function fetchPopularMovies() {}
+
+//RandomTrailer
+export async function fetchMovieTrailers() {}
+
+//Filter
+export async function fetchMoviesForFilter() {}
+
+//Pagination
+export async function fetchDiscoverMovies() {}
