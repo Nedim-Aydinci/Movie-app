108b90072ee93352b7cdb03982f152a939bb67d7
fiag84 <229909100+fiag84@users.noreply.github.com>
2026-05-01 17:08:46 +0200

    updates the Nav component with zustand and can therefore be used in combination with MovieFilter

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,25 +2,31 @@ import { FaHeart, FaUser, FaDice } from "react-icons/fa";
 import "../Styles/Navbar.css";
 import { useState } from "react";
 import { Link } from "react-router";
+import useSearchStore from "../store/SearchStore.js";
 
-function Navbar({ onSearch, onReset }) {
-  //Skickar onSearch som prop så att allt kopplas ihop i App.jsx
-  //När användaren söker skickas query-värdet upp till App.jsx via onSearch-propen
-
-  const [query, setQuery] = useState(""); //Börja med tom sträng
+function Navbar({ onReset }) {
+  //Retrieves state and setters directly from the state store
+  const { query, setQuery, setMode } = useSearchStore();
 
   const handleSearch = (e) => {
-    if (e.key === "Enter") {
-      //Om användaren trycker på enter så skickar onSearch(query), alltså det användaren har skrivit
-      onSearch(query);
-    }
+    if (e.key !== "Enter" || query.trim() === "") return;
+    setMode("search");
+  };
+
+  //Resets all filters and switches back to browse mode.
+  //Called, when the user clicks on logotype or "Clear search".
+  const handleReset = () => {
+    setQuery("");
+    setMode("browse");
+    //calls the reset function from App.jsx if it's passed in as a prop
+    if (onReset) onReset();
   };
 
   return (
     <>
       <nav className="navbar">
         <div className="nav-container">
-          <Link to="/" onClick={onReset}>
+          <Link to="/" onClick={handleReset}>
             <h1 className="logo">
               Movie
               <br />
