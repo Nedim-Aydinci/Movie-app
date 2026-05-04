b586fed69912b57f60ae8627ac34f19ff0177435
emil7233 <emil.svanstrom15@gmail.com>
2026-05-04 13:40:47 +0200

    Added fetch for actors and directors, minor design changes

--- src/Components/MovieInfoCard.jsx
+++ src/Components/MovieInfoCard.jsx
@@ -3,10 +3,13 @@ import star from "../assets/star.svg";
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-export default function MovieInfoCard({ movie }) {
+export default function MovieInfoCard({ movie, credits }) {
   //poster path because movie.poster_path only gives the specific img and not the whole url
   const posterPath = `${IMAGE_BASE_URL}${movie.poster_path}`;
   const backdropPath = `${IMAGE_BASE_URL}${movie.backdrop_path}`;
+
+  const director = credits.crew.find((person) => person.job === "Director");
+  const cast = credits.cast.slice(0, 5);
 
   //Movie info card
   return (
@@ -22,24 +25,37 @@ export default function MovieInfoCard({ movie }) {
 
         <p className="genres">
           {movie.genres.slice(0, 2).map((genre) => (
-            <span key={genre.id} className="genre-tag">
+            <span key={genre.id} className="tag">
               {genre.name}
             </span> //Genres kommer i en array så därav mappar vi igenom de
           ))}
         </p>
 
         <div className="date-and-runtime-wrapper">
-          <p className="movie-info-date">
-            Utgivningsdatum: {movie.release_date}
-          </p>
+          <p className="movie-info-date">Release Date: {movie.release_date}</p>
           <p className="run-time">{movie.runtime} minutes</p>
         </div>
 
         <p className="movie-info-overview">{movie.overview}</p>
 
+        <div className="director-wrapper">
+          <p>Director:</p>
+          <p id="director">{director.name}</p>
+        </div>
+
+        <p>Actors:</p>
+
+        <div className="movie-cast">
+          {cast.map((actor) => (
+            <span key={actor.id} className="tag">
+              {actor.name}
+            </span>
+          ))}
+        </div>
+
         <div className="movie-info-stats">
           {/*rounds the number to get less of extra numbers and it looks better*/}
-          <p>Popularitet: {Math.round(movie.popularity)}</p>
+          <p>Popularity: {Math.round(movie.popularity)}</p>
           <div className="movie-rating">
             <img src={star} alt="rating" className="star-icon" />
             <span>{movie.vote_average.toFixed(1)}</span>
--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,32 +1,43 @@
-import { useParams } from "react-router"
-import { useState, useEffect } from "react"
-import MovieInfoCard from "../Components/MovieInfoCard"
+import { useParams } from "react-router";
+import { useState, useEffect } from "react";
+import MovieInfoCard from "../Components/MovieInfoCard";
 
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
 export default function MoviePage() {
+  const { id } = useParams(); //Hämtar filmID från URL
+  const [movie, setMovie] = useState(null); //Ingen film vald från början
+  const [credits, setCredits] = useState(null); //Regissör och skådespelare
 
-    const { id } = useParams() //Hämtar filmID från URL
-    const [movie, setMovie] = useState(null) //Ingen film vald från början
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${TMDB_TOKEN}`,
+        accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setMovie(data))
+      .catch((error) => console.error("Error fetching movie", error));
 
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${TMDB_TOKEN}`,
-                accept: "application/json",
-            },
-        })
-        .then((res) => res.json())
-        .then((data) => setMovie(data))
-        .catch((error) => console.error("Error fetching movie", error))
-    }, [id])
+    fetch(`https://api.themoviedb.org/3/movie/${id}/credits`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${TMDB_TOKEN}`,
+        accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setCredits(data))
+      .catch((error) => console.error("Error fetching credits", error));
+  }, [id]);
 
-    if(!movie) return <p>Laddar...</p> //Visa laddningstext medan API-svaret hämtas, movie är null tills dess
-    
-    return (
-        <>
-        <MovieInfoCard movie={movie} />
-        </>
-    )
+  if (!movie || !credits) return <p>Laddar...</p>; //Visa laddningstext medan API-svaret hämtas, movie är null tills dess
+
+  return (
+    <>
+      <MovieInfoCard movie={movie} credits={credits} />
+    </>
+  );
 }
